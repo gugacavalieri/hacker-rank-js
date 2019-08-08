65a44ee1246dd328b05d32df68fdc23e933c9e3a
Gustavo Cavalieri <gugacavalieri@gmail.com>
2019-08-07 21:47:37 -0300

    solving arrays/left-rotation

---
+++ arrays/left-rotation/solution.js
@@ -0,0 +1,24 @@
+/**
+ * This solution uses .splice() method
+ *
+ * The complexity is O(n) since we go through all elements of the original array
+ * to construct the new array :)
+ *
+ * @param  {Array} array array containing elements
+ * @param  {Number} d    rotation factor
+ * @return {Array}       Array rotated 'd' positions to the left
+ */
+const rotLeft = (array, d) => {
+  const pivot = d % array.length
+
+  const firstElements = array.slice(pivot, array.length)
+  const lastElements = array.slice(0, pivot)
+
+  return [...firstElements, ...lastElements]
+}
+
+/*
+  we can implement a future solution using only 'for'
+ */
+
+module.exports = { rotLeft }
---
+++ arrays/left-rotation/test.js
@@ -0,0 +1,57 @@
+const { expect } = require('chai')
+const Solution = require('./solution')
+
+describe('arrays/left-rotation.test.js', () => {
+  describe('rotLeft()', () => {
+    it('should rotate array to left 4 positions', () => {
+      // given
+      const array = [1, 2, 3, 4, 5]
+
+      // when
+      const result = Solution.rotLeft(array, 4)
+
+      // then
+      expect(result).to.deep.equal([5, 1, 2, 3, 4])
+    })
+    it('should rotate array to left 0 positions', () => {
+      // given
+      const array = [1, 2, 3, 4, 5]
+
+      // when
+      const result = Solution.rotLeft(array, 0)
+
+      // then
+      expect(result).to.deep.equal([1, 2, 3, 4, 5])
+    })
+    it('should rotate array to left 5 positions', () => {
+      // given
+      const array = [1, 2, 3, 4, 5]
+
+      // when
+      const result = Solution.rotLeft(array, 5)
+
+      // then
+      expect(result).to.deep.equal([1, 2, 3, 4, 5])
+    })
+    it('should rotate array to left 10 positions', () => {
+      // given
+      const array = [41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51]
+
+      // when
+      const result = Solution.rotLeft(array, 10)
+
+      // then
+      expect(result).to.deep.equal([77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77])
+    })
+    it('should rotate empty array', () => {
+      // given
+      const array = []
+
+      // when
+      const result = Solution.rotLeft(array, 1)
+
+      // then
+      expect(result).to.deep.equal([])
+    })
+  })
+})
