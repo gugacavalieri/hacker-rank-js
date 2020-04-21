04bcb30f932cbeb57855ea570f4b81d857b2199f
Gustavo Cavalieri <gugacavalieri@gmail.com>
2020-04-21 09:15:53 -0300

    solve simple-array-sum

---
+++ src/arrays/simple-array-sum/solution.js
@@ -0,0 +1,13 @@
+/**
+ * we are going to use functional programming to solve this
+ * The reduce() method reduces the array to a single value.
+ * The return value of the function is stored in an accumulator (result/total).
+ * @param {*} ar Array containing numbers
+ */
+const simpleArraySum = (ar) => {
+  return ar.reduce((sum, number) => {
+    return Number(sum) + Number(number)
+  }, 0)
+}
+
+module.exports = { simpleArraySum }
